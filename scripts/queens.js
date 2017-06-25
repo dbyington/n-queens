0b87e4267624999e517ae0e21a8d0c1a14949415
Don Byington <donby@blacknblue.com>
2017-06-25 21:33:05 +0200

    save point, before replacing some chunks

--- scripts/queens.js
+++ scripts/queens.js
@@ -1,4 +1,5 @@
 'use strict';
+const Console = console;
 
 function calc (size) {
   if (size === 0) return 0;
@@ -8,33 +9,73 @@ function calc (size) {
 class Queens {
   constructor (max) {
     this.max = max;
-    this.placements = [];
     this.solutions = 0;
   }
   placeQueens () {
+    Console.log('=====================START========================');
     this.tryPlace();
+    Console.log('======================END==========================');
     return this.solutions;
   }
-  tryPlace (depth = 0) {
-    if (depth === this.max) {
+  tryPlace (row = 1, column = 0, major = 0, minor = 0) {
+    if (row > this.max) {
       this.solutions++;
+      Console.log('SOLUTION!:',this.solutions,'(',column.toString(2),major.toString(2),minor.toString(2),')');
       return;
     }
-    for (let c = 0; c < this.max; c++) {
-      if (!this.placements.includes(c)) {
-        let colAdjust = 1;
-        let diag = false;
-        for (let d = depth-1; d >= 0; d--) {
-          if (this.placements[d] === c - colAdjust || this.placements[d] === c + colAdjust++) diag = true;
+    let takenAbove = Array.from((column | major | minor).toString(2)).reverse().slice(0,this.max);
+    // let openSpaces = (~takenAbove) >>> 0);
+    // if (row === 1) {
+    //   let c = column | 1;
+    //   let mj = c << 1;
+    //   let mn = c >>> 1
+    //   Console.log((mj).toString(2));
+    //   Console.log('row:',row,'\ttakenAbove:',takenAbove,'\topenSpaces:',openSpaces.toString(2));
+    //   this.tryPlace(row + 1, c, mj, mn);
+    // }
+    // //let openSpaces = (notTaken | parseInt('1'.repeat(this.max),2));
+    // if (openSpaces) { // > 1, there are open spaces
+    //   Console.log('There are spaces:');
+    //   let c = column | 1;
+    //   let mj = c << 1;
+    //   let mn = c >>> 1
+    //   if (openSpaces % 2) { // it's odd, so column 1 is free
+    //     this.tryPlace(row + 1, c | 1, mj | (major << 1), mn | (minor >>> 1));
+    //   }
+      // if it's not odd, count up in binary to the max size and if the openSpaces is evenly divisible by tryColumn thats a space
+    // let tryColumn = 2;
+    Console.log('ROW:',row,'\ttakenAbove:',takenAbove); //,'\topenSpaces:',openSpaces.toString(2));
+    // for (let tryColumn = this.max; tryColumn > 0; tryColumn++) {
+      // Console.log('trying column:',tryColumn,openSpaces%tryColumn);
+    for (let i = 0; i < this.max; i++) {
+      Console.log('for: i',i);
+      if (!takenAbove || takenAbove[i] === 0) {
+        Console.log('Row',row,'Column',i,'is free.', takenAbove.toString(2));
+        let c = (1 << i) | column;
+        let mj = i << 1;
+        if (c < this.max - 1) {
+          mj = (1 << i << 1);
         }
-        if (!diag) {
-          this.placements.push(c);
-          this.tryPlace(depth+1);
-          this.placements.pop();
+        if (major << 1 < this.max) {
+          mj = mj | (major << 1) ;
         }
+        let mn = (c >>> 1) | (minor >>> 1);
+        Console.log('row',row,'descriptor: columns:',
+         c.toString(2),
+          '\tmajor:',mj.toString(2),
+          '\tminor',mn.toString(2));
+
+        this.tryPlace(row + 1, c, mj, mn);
+      } else {
+        // Console.log('NOPE column:',tryColumn);
       }
+      if (row === 1) Console.log('\n\n////////////////  back at row 1 \\\\\\\\\\\\\\\\\\\\\\\\');
+      // Console.log('Back at row:',row,'column:',tryColumn+1);
     }
+  // }
   }
+
 }
+Console.log(calc(5));
 
 module.exports = calc;
