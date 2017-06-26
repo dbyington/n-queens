ffb5e7a4f96edf37f1560af562618d5ab94ba266
Don Byington <donby@blacknblue.com>
2017-06-26 11:33:32 +0200

    feat: solved bitwise using scratch file q2.js

--- scripts/queens.js
+++ scripts/queens.js
@@ -1,81 +1,58 @@
 'use strict';
 const Console = console;
 
-function calc (size) {
-  if (size === 0) return 0;
-  const queen = new Queens(size);
-  return queen.placeQueens();
-}
-class Queens {
-  constructor (max) {
-    this.max = max;
-    this.solutions = 0;
-  }
-  placeQueens () {
-    Console.log('=====================START========================');
-    this.tryPlace();
-    Console.log('======================END==========================');
-    return this.solutions;
-  }
-  tryPlace (row = 1, column = 0, major = 0, minor = 0) {
-    if (row > this.max) {
-      this.solutions++;
-      Console.log('SOLUTION!:',this.solutions,'(',column.toString(2),major.toString(2),minor.toString(2),')');
-      return;
-    }
-    let takenAbove = Array.from((column | major | minor).toString(2)).reverse().slice(0,this.max);
-    // let openSpaces = (~takenAbove) >>> 0);
-    // if (row === 1) {
-    //   let c = column | 1;
-    //   let mj = c << 1;
-    //   let mn = c >>> 1
-    //   Console.log((mj).toString(2));
-    //   Console.log('row:',row,'\ttakenAbove:',takenAbove,'\topenSpaces:',openSpaces.toString(2));
-    //   this.tryPlace(row + 1, c, mj, mn);
-    // }
-    // //let openSpaces = (notTaken | parseInt('1'.repeat(this.max),2));
-    // if (openSpaces) { // > 1, there are open spaces
-    //   Console.log('There are spaces:');
-    //   let c = column | 1;
-    //   let mj = c << 1;
-    //   let mn = c >>> 1
-    //   if (openSpaces % 2) { // it's odd, so column 1 is free
-    //     this.tryPlace(row + 1, c | 1, mj | (major << 1), mn | (minor >>> 1));
-    //   }
-      // if it's not odd, count up in binary to the max size and if the openSpaces is evenly divisible by tryColumn thats a space
-    // let tryColumn = 2;
-    Console.log('ROW:',row,'\ttakenAbove:',takenAbove); //,'\topenSpaces:',openSpaces.toString(2));
-    // for (let tryColumn = this.max; tryColumn > 0; tryColumn++) {
-      // Console.log('trying column:',tryColumn,openSpaces%tryColumn);
-    for (let i = 0; i < this.max; i++) {
-      Console.log('for: i',i);
-      if (!takenAbove || takenAbove[i] === 0) {
-        Console.log('Row',row,'Column',i,'is free.', takenAbove.toString(2));
-        let c = (1 << i) | column;
-        let mj = i << 1;
-        if (c < this.max - 1) {
-          mj = (1 << i << 1);
-        }
-        if (major << 1 < this.max) {
-          mj = mj | (major << 1) ;
-        }
-        let mn = (c >>> 1) | (minor >>> 1);
-        Console.log('row',row,'descriptor: columns:',
-         c.toString(2),
-          '\tmajor:',mj.toString(2),
-          '\tminor',mn.toString(2));
+function calc (mx) {
+  if (mx === 0) return 0;
+  let q = 0;
 
-        this.tryPlace(row + 1, c, mj, mn);
-      } else {
-        // Console.log('NOPE column:',tryColumn);
+  const pQ = (r = 1, c = 0, j = 0, n = 0) => {
+    if (r > mx) {
+      q++;
+      return true;
+    }
+
+    let mR = (c | j | n);
+    for (let s = 1; s <= mx; s++) {
+
+      let mC, mJ, mN, oJ = 0;
+      if (mR % 2 === 0) {
+
+        mC = 1 << s-1;
+        // Create our major
+        // if our major will over run the length of our board
+        // clip it off
+        if (mC >= (1 << mx-1)) {
+          mJ = 0;
+        } else {
+          mJ = mC << 1;
+        }
+        // check to see if the original major will run over
+        oJ = j << 1;
+
+        if (oJ >= (1 << mx)) {
+          oJ = oJ - (1<<mx);
+        }
+        mJ = mJ | oJ;
+
+        // create our minor from our column
+        mN = mC >>> 1;
+        let tmN = n >>> 1;
+        mN = mN | (n >>> 1);
+        mC = mC | c;
+
+        pQ(r+1, mC, mJ, mN);
       }
-      if (row === 1) Console.log('\n\n////////////////  back at row 1 \\\\\\\\\\\\\\\\\\\\\\\\');
-      // Console.log('Back at row:',row,'column:',tryColumn+1);
+      mR = mR >>> 1;
     }
-  // }
-  }
-
+    return q;
+  };
+  return pQ();
 }
-Console.log(calc(5));
+
+const size = 10;
+const st = Date.now()/1000;
+const p = calc(size);
+const end = Date.now()/1000;
+Console.log(`Queens size ${size}: ${p} (in ${end - st} seconds)`);
 
 module.exports = calc;
